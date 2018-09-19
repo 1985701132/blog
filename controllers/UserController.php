9713daaf4eae1ed9162b40fe33d1f52680bfd0fb
1985701132@qq.com <1985701132@qq.com>
2018-09-19 11:17:18 +0800

    头像裁切

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,5 +1,6 @@
 <?php
     namespace controllers;
+    use Intervention\Image\ImageManagerStatic as Image;
     use models\User;
 
     class UserController{
@@ -126,6 +127,15 @@
             //上传新头像
             $upload = \libs\Uploader::make();
             $path = $upload->upload('avatar','avatar');
+
+            //裁切图片
+            $image = Image::make(ROOT . 'public/uploads/'.$path);
+
+            // 注意：Crop 参数必须是整数，所以需要转成整数：(int)
+            $image->crop((int)$_POST['w'],(int)$_POST['h'],(int)$_POST['x'],(int)$_POST['y']);
+
+            //保存时覆盖原图
+            $image->save(ROOT . 'public/uploads/'.$path);
 
             //保存到user表中
             $model = new \models\User;
